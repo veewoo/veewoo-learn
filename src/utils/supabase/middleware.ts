2b53af74d2eb83c5291f0d89c3d414928a341b90
veewoo <viethung8074@gmail.com>
2026-04-30 15:42:08 +0700

    refactor: update environment variable for Supabase anon key

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -16,7 +16,7 @@ export const createClient = (request: NextRequest) => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const supabase = createServerClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        process.env.SUPABASE_ANON_KEY!,
         {
             cookies: {
                 getAll() {
