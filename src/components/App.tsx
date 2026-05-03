e0cd8816d6189200c81fc60b368467f434e2b8d5
veewoo <viethung8074@gmail.com>
2026-05-03 14:46:51 +0700

    refactor: remove PWAInstall component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,6 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import dynamic from "next/dynamic";
-
-// Dynamically import the PWA installer component to ensure it only runs client-side
-const PWAInstall = dynamic(() => import("./PWAInstall"), {
-  ssr: false,
-});
 
 // Create a client with aggressive anti-caching settings for iOS Safari compatibility
 const queryClient = new QueryClient({
@@ -25,9 +19,6 @@ type Props = {
 
 export default function App({ children }: Props) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <PWAInstall />
-      {children}
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
